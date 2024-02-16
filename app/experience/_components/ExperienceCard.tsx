f7472061a568dbd92921567b030cf5097fc48ecd
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-16 02:42:02 -0300

    feat: developed experience section

--- app/experience/_components/ExperienceCard.tsx
+++ app/experience/_components/ExperienceCard.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { TechIcon } from '@/components/icons';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { useLanguage } from '@/hooks/useLanguage';
 
 export const ExperienceCard = () => {
 	const language = useLanguage((state) => state.language);
 
 	return (
-		<Card className='md:w-[480px] bg-neutral-800 text-white mx-auto mt-12 led-effect border-green-400'>
-			<CardHeader className='text-lg px-12 text-center'>
+		<Card className='w-full sm:w-[580px] bg-neutral-800 text-white mx-auto mt-12 led-effect border-green-400'>
+			<CardHeader className='text-base sm:text-lg px-6 sm:px-12 text-center'>
 				{language === 'pt'
 					? 'Desenvolvedor FrontEnd em CADFEI: Site Institucional de Carreira Docente'
 					: 'FrontEnd Developer at CADFEI: Professor Career Institucional Website'}
@@ -18,20 +18,29 @@ export const ExperienceCard = () => {
 				<div className='w-full flex items-center justify-center'>
 					<TechIcon
 						color='green'
-						selectedIcon='javascript'
+						selectedIcon='typescript'
 						className='w-12 h-12'
 					/>
 					<TechIcon
 						color='green'
-						selectedIcon='next'
+						selectedIcon='react'
+						className='w-12 h-12'
+					/>
+					<TechIcon
+						color='green'
+						selectedIcon='css'
 						className='w-12 h-12'
 					/>
 				</div>
 				<p className='text-sm text-justify'>
-					FrontEnd Development using React, TypeScript, Redux, CSS & Vite. CADFEI is FEI professor's
-					institucional website.
+					{language === 'pt'
+						? 'Desenvolvimento FrontEnd do CADFEI, o website institucional dos professores da FEI. As principais ferramentas utilizadas durante o desenvolvimento são React, TypeScript, Redux, CSS & Vite.'
+						: "FrontEnd Development of CADFEI, FEI's professor's institucional website. The tech stack used while developing is, mainly, React, TypeScript, Redux, CSS & Vite."}
 				</p>
 			</CardContent>
+			<CardFooter className='flex justify-center text-white/50'>
+				{language === 'pt' ? 'Setembro/2023 - Agora' : 'September/2023 - Now'}
+			</CardFooter>
 		</Card>
 	);
 };
--- app/experience/_components/ExperienceSection.tsx
+++ app/experience/_components/ExperienceSection.tsx
@@ -7,11 +7,60 @@ export const ExperienceSection = () => {
 	const language = useLanguage((state) => state.language);
 
 	return (
-		<section className='w-full mt-32 text-white'>
+		<section className='px-4 mx-auto max-w-screen-lg w-full mt-24 text-white'>
 			<h1 className='font-semibold text-3xl md:text-5xl text-center'>
 				{language === 'pt' ? 'Experiência' : 'Experience'}
 			</h1>
 			<ExperienceCard />
+			<p className='text-justify mt-12'>
+				{language === 'en' ? (
+					<>
+						Before even entering college, my interest in programming had already been awakened, what made me
+						watch courses on YouTube, specially about web development and programming logic. This initial
+						passion was the foundation that propelled me to the next step: college. In university, my
+						enthusiasm only grew as I delved deeper into the world of programming. Every challenge I
+						overcame increased my certainty that I was on the right path.{' '}
+						<span className='highlighted'>
+							Software development wasn't just a career I was pursuing, it was my passion.
+						</span>
+					</>
+				) : (
+					<>
+						Antes mesmo de entrar na faculdade, meu interesse em programação já havia sido despertado, o que
+						me levou a assistir cursos no YouTube, especialmente sobre desenvolvimento web e lógica de
+						programação. Essa paixão inicial foi a base que me impulsionou para o próximo passo: a
+						faculdade. Na universidade, meu entusiasmo só cresceu à medida que mergulhei mais fundo no mundo
+						da programação. Cada desafio superado aumentava minha certeza de que eu estava no caminho certo.{' '}
+						<span className='highlighted'>
+							O desenvolvimento de software não era apenas uma carreira que eu estava seguindo, era minha
+							paixão.
+						</span>
+					</>
+				)}
+			</p>
+			<br />
+			<p className='text-justify'>
+				{language === 'en' ? (
+					<>
+						Over time, I looked for courses to increase my knowledge, that's why I started studying
+						development with JavaScript, which led me to TypeScript, ReactJS and NodeJS. In fact,{' '}
+						<span className='highlighted'>
+							this portfolio was developed using ReactJS, TypeScript, Tailwind and NextJS
+						</span>
+						, stack which, nowadays, is the one I use the most.
+					</>
+				) : (
+					<>
+						Ao longo do tempo, procurei por cursos para aumentar meu conhecimento, por isso comecei a
+						estudar desenvolvimento com JavaScript, o que me levou ao TypeScript, ReactJS e NodeJS.
+						Inclusive,{' '}
+						<span className='highlighted'>
+							este portfólio foi desenvolvido usando ReactJS, TypeScript, Tailwind e NextJS
+						</span>
+						, stack que, atualmente, é a que eu mais utilizo.
+					</>
+				)}
+			</p>
 		</section>
 	);
 };
