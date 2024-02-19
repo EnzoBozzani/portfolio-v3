910f063f2e18255d16b8d8fb2f1b73abdd9133b5
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-19 16:21:23 -0300

    feat: conclusion of experience page development

--- app/experience/_components/CertificateCard.tsx
+++ app/experience/_components/CertificateCard.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { CarouselItem } from '@/components/ui/carousel';
 import Image from 'next/image';
 
 interface CertificateCardProps {
@@ -5,23 +8,27 @@ interface CertificateCardProps {
 	title: string;
 	description: string;
 	date: string;
+	givenBy: string;
 }
 
-export const CertificateCard = ({ imgSrc, title, description, date }: CertificateCardProps) => {
+export const CertificateCard = ({ imgSrc, title, description, date, givenBy }: CertificateCardProps) => {
 	return (
-		<div className='rounded-lg w-full border-2 border-green-400 bg-neutral-900 text-white flex flex-col gap-y-4'>
-			<Image
-				src={imgSrc}
-				alt={`${title} img`}
-				width={300}
-				height={300}
-				className='w-full h-auto rounded-t-lg'
-			/>
-			<div className='space-y-1 p-3 text-justify'>
-				<p className='text-sm md:text-lg'>{title}</p>
-				<p className='text-xs text-white/50'>{date}</p>
-				<p className='text-sm md:text-sm pt-4'>{description}</p>
+		<CarouselItem className='flex items-center justify-center'>
+			<div className='rounded-lg w-full border-2 border-green-400 bg-neutral-900 text-white flex flex-col gap-y-4'>
+				<Image
+					src={imgSrc}
+					alt={`${title} img`}
+					width={300}
+					height={300}
+					className='w-full h-auto rounded-t-lg'
+				/>
+				<div className='space-y-1 p-3 text-justify'>
+					<p className='text-sm md:text-lg'>{title}</p>
+					<p className='text-xs text-white/50'>{date}</p>
+					<p className='text-sm md:text-sm pt-4'>{description}</p>
+					<p className='text-xs text-white/50 text-center pt-6'>{givenBy}</p>
+				</div>
 			</div>
-		</div>
+		</CarouselItem>
 	);
 };
--- app/experience/_components/CertificatesSection.tsx
+++ app/experience/_components/CertificatesSection.tsx
@@ -13,36 +13,53 @@ export const CertificatesSection = () => {
 			<h1 className='font-semibold text-3xl md:text-5xl text-center'>
 				{language === 'pt' ? 'Certificados' : 'Certificates'}
 			</h1>
-			<Carousel className='mx-auto w-[70%] sm:w-[500px] mt-6'>
+			<Carousel className='mx-auto w-[70%] sm:w-[500px] mt-3'>
 				<CarouselContent>
-					<CarouselItem className='flex items-center justify-center'>
-						<CertificateCard
-							imgSrc='/FEI.png'
-							title={
-								language === 'pt'
-									? 'Certificado de Melhor colocação no Processo Seletivo da FEI'
-									: "Certificate of Best Placement in FEI's Selection Process"
-							}
-							description={language === 'pt' ? 'xsmdniumd' : 'demdim'}
-							date='03/2022'
-						/>
-					</CarouselItem>
-					<CarouselItem className='flex items-center justify-center'>
-						<CertificateCard
-							imgSrc='/FullStackJS.png'
-							title={language === 'pt' ? '' : "Certificate of Best Placement in FEI's Selection Process"}
-							description={language === 'pt' ? 'xsmdniumd' : 'demdim'}
-							date='03/2022'
-						/>
-					</CarouselItem>
-					<CarouselItem className='flex items-center justify-center'>
-						<CertificateCard
-							imgSrc='/DecolaTech.png'
-							title={language === 'pt' ? '' : "Certificate of Best Placement in FEI's Selection Process"}
-							description={language === 'pt' ? 'xsmdniumd' : 'demdim'}
-							date='03/2022'
-						/>
-					</CarouselItem>
+					<CertificateCard
+						imgSrc='/FullStackJS.png'
+						title={
+							language === 'pt'
+								? 'Certificado de Conclusão do curso Full Stack JavaScript'
+								: 'Conclusion Certificate of Full Stack JavaScript course'
+						}
+						description={
+							language === 'pt'
+								? 'Certificado concedido pela conclusão do curso Full Stack JavaScript, o qual ensina a utilizar ferramentas no desenvolvimento web FullStack, como HTML, CSS, BootStrap, SASS, JavaScript, TypeScript, ReactJS, NextJS, NodeJS, ExpressJS, PostgreSQL. Concedido por OneBitCode'
+								: 'Certificate awarded upon completion of the Full Stack JavaScript course, which teaches the use of tools in FullStack web development, such as HTML, CSS, BootStrap, SASS, JavaScript, TypeScript, ReactJS, NextJS, NodeJS, ExpressJS, PostgreSQL. Granted by OneBitCode.'
+						}
+						date='11/2023'
+						givenBy='OneBitCode'
+					/>
+					<CertificateCard
+						imgSrc='/FEI.png'
+						title={
+							language === 'pt'
+								? 'Certificado de Melhor colocação no Processo Seletivo da FEI'
+								: "Certificate of Best Placement in FEI's Selection Process"
+						}
+						description={
+							language === 'pt'
+								? "Certificate granted by University Center of FEI for the best placement in FEI's 2022 First Semester Selection Process"
+								: 'Certificado concedido pelo Centro Universitário FEI pela melhor colocação no Processo Seletivo da FEI no Primeiro Semestre de 2022'
+						}
+						date='03/2022'
+						givenBy='FEI'
+					/>
+					<CertificateCard
+						imgSrc='/DecolaTech.png'
+						title={
+							language === 'pt'
+								? 'Certificado de Conclusão do Bootcamp DecolaTech'
+								: 'Conclusion Certificate of DecolaTech Bootcamp'
+						}
+						description={
+							language === 'pt'
+								? 'Certificado de conclusão do Bootcamp DecolaTech 2024, o qual ensina diversos conceitos e fundamentos do desenvolvimento utilizando C# e .NET. Concedido por Avanade.'
+								: 'Certificate of completion of the DecolaTech 2024 Bootcamp, which teaches various concepts and fundamentals of development using C# and .NET. Granted by Avanade.'
+						}
+						date='12/2023'
+						givenBy='Avanade'
+					/>
 				</CarouselContent>
 				<CarouselPrevious className='bg-neutral-900' />
 				<CarouselNext className='bg-neutral-900' />
